b129301948dbc15bbe5469184afaaa6489cc03d9
Albina Lupanova <unbrella77@mail.ru>
2023-12-03 11:21:52 +0300

    Hw 3 first attempt

---
+++ hw 3/app/service.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def ni_hao():
+    return "Ni Hao"
+
+if __name__ == "__main__":
+    app.run(port=8080, host='0.0.0.0')
+
